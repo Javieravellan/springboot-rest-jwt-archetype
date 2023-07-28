8261df9cb60eb10b7a2e7b07980c01800f7a1bce
Javier Chiquito Avellán <wchiquito@eclipsoft.com>
2023-07-28 11:59:04 -0500

    Nuevas correcciones

--- src/main/resources/archetype-resources/src/main/java/__mainClass__.java
+++ src/main/resources/archetype-resources/src/main/java/__mainClass__.java
@@ -4,12 +4,14 @@
 package ${package};
 
 import com.github.cloudyrock.spring.v5.EnableMongock;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @SpringBootApplication
 @Log4j2
@@ -23,11 +25,12 @@ public class ${mainClass} {
 		var app = new SpringApplication(${mainClass}.class);
 		${mainClass}.addDefaultProfile(app);
 		Environment env = app.run(args).getEnvironment();
+		logProfilesActives(env);
 	}
 
 	private static void addDefaultProfile(SpringApplication app) {
 		Map<String, Object> defProperties = new HashMap<>();
-		defProperties.put(SPRING_PROFILE_DEFAULT, "default,dev");
+		defProperties.put("spring.profiles.default", "default,dev");
 		app.setDefaultProperties(defProperties);
 	}
 
