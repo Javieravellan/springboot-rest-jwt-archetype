cefafbd7d7c07d310ea0f5f7055d7057b26d1ccf
Javier Chiquito Avellán <wchiquito@eclipsoft.com>
2023-08-15 15:41:43 -0500

    Refactor ControllerAdvice

--- src/main/resources/archetype-resources/src/main/java/exception/GlobalApiExceptionHandler.java
+++ src/main/resources/archetype-resources/src/main/java/exception/GlobalApiExceptionHandler.java
@@ -10,12 +10,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.util.WebUtils;
+
+import java.util.ArrayList;
 
 @ControllerAdvice
 @Log4j2
@@ -26,6 +29,7 @@ public class GlobalApiExceptionHandler {
             GenericException.class,
             AccessDeniedException.class,
             MethodArgumentNotValidException.class,
+            HttpRequestMethodNotSupportedException.class
     })
     public final ResponseEntity<ApiError> handleException(Exception ex, WebRequest webRequest) {
         HttpHeaders headers = new HttpHeaders();
@@ -45,22 +49,13 @@ public class GlobalApiExceptionHandler {
             return handleExceptionInternal(bre, apiError, headers, apiError.getStatusText(), webRequest);
         }
         else if(ex instanceof MethodArgumentNotValidException me) {
-            var errors = ApiError.builder()
-                    .statusCode(HttpStatus.BAD_REQUEST.value())
-                    .message("Error de validación de campos")
-                    .statusText(HttpStatus.BAD_REQUEST)
-                    .build();
-            setFieldErrorsIntoApiErrorObject(errors, me);
-            return handleExceptionInternal(me, errors, headers, HttpStatus.BAD_REQUEST, webRequest);
+            return this.handleMethodArgumentNotValidException(me, headers, webRequest);
         }
         else if (ex instanceof GenericException ge) {
-            var apiError = ApiError.builder()
-                    .statusCode(ge.getStatus().value())
-                    .message(ge.getMessage())
-                    .statusText(ge.getStatus())
-                    .build();
-
-            return handleExceptionInternal(ge, apiError, headers, ge.getStatus(), webRequest);
+            return this.handleGenericException(ge, headers, webRequest);
+        }
+        else if (ex instanceof HttpRequestMethodNotSupportedException exNotSupported) {
+            return this.handleMethodNotAllowedException(exNotSupported, headers, webRequest);
         }
         else {
             var error = ApiError.builder()
@@ -80,6 +75,39 @@ public class GlobalApiExceptionHandler {
         var cause = (NotFoundException) ex.getCause();
         var apiError = new ApiError(cause.getStatusCode(), ex.getMessage(), status.value());
         return handleExceptionInternal(ex, apiError, headers, status, webRequest);
+    }
+
+    public ResponseEntity<ApiError> handleMethodNotAllowedException(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, WebRequest webRequest) {
+        var httpStatus = HttpStatus.valueOf(ex.getStatusCode().value());
+        var message = String.format("Método '%s' no soportado. Solo se admiten los métodos: '%s'.", ex.getMethod(), ex.getSupportedHttpMethods());
+        var error = ApiError.builder()
+                .message(message)
+                .statusCode(httpStatus.value())
+                .statusText(httpStatus)
+                .build();
+
+        return handleExceptionInternal(ex, error, headers, httpStatus, webRequest);
+    }
+
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpHeaders headers, WebRequest webRequest) {
+        var errors = ApiError.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .message("Error de validación de campos")
+                .statusText(HttpStatus.BAD_REQUEST)
+                .fieldErrors(new ArrayList<>())
+                .build();
+        setFieldErrorsIntoApiErrorObject(errors, ex);
+        return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, webRequest);
+    }
+
+    public ResponseEntity<ApiError> handleGenericException(GenericException ex, HttpHeaders headers, WebRequest webRequest) {
+        var apiError = ApiError.builder()
+                .statusCode(ex.getStatus().value())
+                .message(ex.getMessage())
+                .statusText(ex.getStatus())
+                .build();
+
+        return handleExceptionInternal(ex, apiError, headers, ex.getStatus(), webRequest);
     }
 
     protected ResponseEntity<ApiError> handleExceptionInternal(Exception ex, ApiError body, HttpHeaders headers, HttpStatus status, WebRequest request) {
