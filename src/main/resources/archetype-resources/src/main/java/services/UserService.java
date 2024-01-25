e9cfe94d111c3fbac41c2c2f9429c39b296badce
Javier Chiquito Avellán <wchiquito@eclipsoft.com>
2024-01-25 10:04:44 -0500

    Add UserMapper.java; update UserService to use UserMapper

--- src/main/resources/archetype-resources/src/main/java/services/UserService.java
+++ src/main/resources/archetype-resources/src/main/java/services/UserService.java
@@ -9,6 +9,7 @@ import ${package}.domain.User;
 import ${package}.domain.enums.ERole;
 import ${package}.repository.RoleRepository;
 import ${package}.repository.UserRepository;
+import ${package}.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,11 +24,15 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
     private final RoleRepository roleRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository, PasswordEncoder encoder, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder encoder,
+                       RoleRepository roleRepository,
+                       UserMapper userMapper) {
         this.userRepository = userRepository;
         this.encoder = encoder;
         this.roleRepository = roleRepository;
+        this.userMapper = userMapper;
     }
 
     public Boolean existsByUsername(String username) {
@@ -35,23 +40,27 @@ public class UserService {
     }
 
     public void registerUser(SignUpRequest request) {
-        User user = new User(request.getUsername(), encoder.encode(request.getPassword()));
+        request.setPassword(encoder.encode(request.getPassword()));
+        User user = userMapper.toEntity(request);
 
-        Set<ERole> strRoles = request.getAuthorities();
-        Set<Role> roles = new HashSet<>();
+        user.setAuthorities(this.mapAuthorities(request.getAuthorities()));
 
-        if (strRoles == null) {
+        userRepository.save(user);
+    }
+
+    private Set<Role> mapAuthorities(Set<ERole> eRoles) {
+        final Set<Role> roles = new HashSet<>();
+        if (eRoles == null) {
             Role userRole = roleRepository.findByName(ERole.ROLE_USER)
                     .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
             roles.add(userRole);
         } else {
-            strRoles.forEach(role -> {
+            eRoles.forEach(role -> {
                 Role currRole = roleRepository.findByName(role).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                 roles.add(currRole);
             });
         }
 
-        user.setAuthorities(roles);
-        userRepository.save(user);
+        return roles;
     }
 }
